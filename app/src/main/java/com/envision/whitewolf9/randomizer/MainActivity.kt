3825814435cd885146425149b66e880bbf06fda5
Saurav Sanyal <sanyal.s271@gmail.com>
2019-11-28 05:46:40 +0530

    Initial commit

---
+++ app/src/main/java/com/envision/whitewolf9/randomizer/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.envision.whitewolf9.randomizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = java.util.Random().nextInt(seekBar.progress)+1
+            resultTextView.text = rand.toString()
+        }
+    }
+}
